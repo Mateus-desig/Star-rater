3074a1d8456f51431e98f29e30918343cee9fb59
Mateus <40679395+Mateus-desig@users.noreply.github.com>
2022-05-08 22:01:17 -0300

    Add files via upload

---
+++ js/StarRater.js
@@ -0,0 +1,77 @@
+class StarRater extends HTMLElement{
+    constructor(){
+        super()
+
+        this.build()
+    }
+
+    build(){
+        const shadow = this.attachShadow({mode: 'open'})
+
+        shadow.appendChild(this.styles())
+
+        const rater = this.createRater()
+        this.stars = this.createStars()
+
+        this.stars.forEach(star => rater.appendChild(star))
+
+        this.resetRating()
+
+        shadow.appendChild(rater)
+    }
+
+    createRater() {
+        const rater = document.createElement('div')
+        rater.classList.add('star-rater')
+        rater.addEventListener('mouseout', this.resetRating.bind(this))
+
+        return rater
+    }
+
+    createStars(){
+        const crateStar = (_, id) => {
+            const star = document.createElement('span')
+            star.classList.add('star')
+            star.setAttribute('data-value', Number(id) +1)
+            star.innerHTML = '&#9733;'
+
+            star.addEventListener('click', this.setRating.bind(this))
+            star.addEventListener('mouseover', this.ratingHover.bind(this))
+            return star
+        }
+
+        return Array.from({length: 5}, crateStar)
+    }
+
+    resetRating(){
+        this.currentRatingValue = this.getAttribute('data-rating') || 0
+        this.hightlightRating()
+    }
+
+    setRating(event){
+        this.setAttribute('data-rating', event.currentTarget.getAttribute('data-value'))
+    }
+
+    ratingHover(event){
+        this.currentRatingValue = event.currentTarget.getAttribute('data-value')
+        this.hightlightRating()
+    }
+
+    hightlightRating(){
+        this.stars.forEach( star => {
+            star.style.color = this.currentRatingValue >= star.getAttribute('data-value')
+            ? 'yellow'
+            : 'gray'
+        })
+    }
+
+    styles(){
+        const style = document.createElement('link')
+        style.rel = 'stylesheet'
+        style.href = 'css/style.css'
+    
+        return style
+    }
+}
+
+customElements.define('star-rater', StarRater)
